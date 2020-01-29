2838986516fb3cd2a07789b25f5bd6d921bba861
Cjanesk1 <54916560+Cjanesk1@users.noreply.github.com>
2020-01-29 15:05:29 -0700

    Add files via upload

---
+++ bubbleswap.java
@@ -0,0 +1,23 @@
+
+public class bubbleswap
+{
+    public static void main(String[]args) {
+    int [] myArray = {24,-12,1000,-3,0,0,-45,67,312,82,4,9,1001,45,56,-1002};  
+    bubleSort(myArray);
+    System.out.println(java.util.Arrays.toString(myArray));
+    }
+    public static void swap(int[] A, int i, int j){
+    int temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+    }
+    public static void bubleSort(int[]A){
+    for(int i=0;i<A.length;i++){
+        for(int j=0;j<A.length-1;j++){
+            if(A[j]>A[j+1]){
+                swap(A,j,j+1);
+            }
+        }
+    }
+    }
+}  
